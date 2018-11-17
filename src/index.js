223cbcdb5dc1f5dae2aeaf48232a5f4d3c6634f0
andy <thamandyy@gmail.com>
2018-11-17 04:01:34 -0500

    reorganized everything

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render, ReactDOM } from "react-dom";
+import { Provider } from "react-redux";
+// import store from "./js/store/index";
+import App from "./components/App.js";
+// ReactDOM.render(
+// 	<Provider store={store}>
+// 		<App />
+// 	</Provider>,
+// 	document.getElementById("root")
+// );
+
+const rootEl = document.getElementById('root');
+
+
+render(<App />, rootEl);
+
+if (module.hot) {
+	module.hot.accept();
+}
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+
+const config = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+
+      {
+        test: /\.js[x]?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['env', 'react']
+      }
+        
+      },
+      {
+        test:/\.(s*)css$/,
+        use: ExtractTextWebpackPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader']
+        })
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [{
+            loader: 'url-loader',
+            options: {
+                limit: 8000, // Convert images < 8kb to base64 strings
+                name: 'images/[hash]-[name].[ext]'
+            }
+        }]
+      }
+    ]
+  },
+  plugins: [
+  new ExtractTextWebpackPlugin('style.css')
+  ]
+};
+
+module.exports = config
